05776de4fe3cd6fb3a6c2d8863bd2798785730fa
Bounthong Jean-Luc <jeanluc.bounthong@gmail.com>
2014-07-18 00:44:40 +0000

    add dummy test for testing

--- tests/test.cc
+++ tests/test.cc
@@ -1,5 +1,15 @@
 #include <gtest/gtest.h>
 
+TEST(DummyTest, DummyOK)
+{
+  ASSERT_TRUE(true);
+}
+
+TEST(DummyTest, DummyKO)
+{
+  ASSERT_TRUE(false);
+}
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);

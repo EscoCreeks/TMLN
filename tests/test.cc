94c472aec0c7ae2de30b9d8801057062f26e6503
Bounthong Jean-Luc <jeanluc.bounthong@gmail.com>
2014-07-18 00:50:59 +0000

    strip useless test

--- tests/test.cc
+++ tests/test.cc
@@ -1,15 +1,5 @@
 #include <gtest/gtest.h>
 
-TEST(DummyTest, DummyOK)
-{
-  ASSERT_TRUE(true);
-}
-
-TEST(DummyTest, DummyKO)
-{
-  ASSERT_TRUE(false);
-}
-
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);

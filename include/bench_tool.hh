2609468a019f826aae327874cd837b799d484614
Bounthong Jean-Luc <jeanluc.bounthong@gmail.com>
2014-07-26 16:43:47 +0000

    fix bench tool early free

--- include/bench_tool.hh
+++ include/bench_tool.hh
@@ -1,6 +1,5 @@
-#include <string>
-#include <iostream>
 #include <chrono>
+#include <common.hh>
 
 #ifndef BENCH_TOOL_HH
 # define BENCH_TOOL_HH
@@ -11,17 +10,16 @@ using std::chrono::nanoseconds;
 using std::chrono::high_resolution_clock;
 
 struct time_guard {
-  time_guard(const std::string& m) {}
-  // high_resolution_clock::time_point      t0;
-  // const std::string&    msg;
-  // time_guard(const std::string& m) : t0(high_resolution_clock::now()), msg(m) {}
-  // ~time_guard() {
-  //   high_resolution_clock::time_point    t1 = high_resolution_clock::now();
-  //   std::cout << msg
-  //             << double(duration_cast<nanoseconds>(t1 - t0).count())/1000000000
-  //             << "s"
-  //             << std::endl;
-  // }
+  high_resolution_clock::time_point      t0;
+  const std::string msg;
+  time_guard(const std::string& m) : t0(high_resolution_clock::now()), msg(m) {}
+  ~time_guard() {
+    high_resolution_clock::time_point    t1 = high_resolution_clock::now();
+    std::cout << msg
+              << double(duration_cast<nanoseconds>(t1 - t0).count())/1000000000
+              << "s"
+              << std::endl;
+  }
 };
 
 #endif /* !BENCH_TOOL_HH */

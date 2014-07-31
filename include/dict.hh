cdf956e7510797b4feb8ce60f21d0c6b019191f8
Christophe ESCOBAR <christophe.esco@gmail.com>
2014-07-31 23:31:20 +0200

    Documentation on dict.hh

--- include/dict.hh
+++ include/dict.hh
@@ -3,12 +3,21 @@
 
 #include <common.hh>
 
+/**
+ * An entry in the dictionnary of words. This struct is used to stock all the
+ * words from the file given to TextMiningCompiler. From the vector of Entry,
+ * a TrieBuilder is then created.
+ */
 struct Entry
 {
-  std::string str;
-  int freq;
+  std::string str; /**< Entry word. */
+  int freq; /**< Entry frequence. */
 };
 
+/**
+ * Parse the file and create a vector of Entry.
+ * @return The vector of entries of the file
+ */
 std::vector<Entry> ParseDict(std::ifstream& filePath);
 
 #endif /* !DICT_HH */
